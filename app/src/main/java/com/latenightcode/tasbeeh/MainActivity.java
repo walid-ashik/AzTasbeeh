c63f4b11715e19b6eccb30d07a49a92404fe8ef3
walid-ashik <walid.al.ashik@gmail.com>
2018-03-20 16:32:27 +0600

    Add ShowCaseView dependency

--- app/src/main/java/com/latenightcode/tasbeeh/MainActivity.java
+++ app/src/main/java/com/latenightcode/tasbeeh/MainActivity.java
@@ -84,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //setting tour guide of app
+
     }
 
     private void getTasbeehNameAndChangeTextViewValue() {
